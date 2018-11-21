72c3472552f70a35761cd8a827dc44014c1abb52
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2018-11-21 16:01:36 -0500

    add main and collection files

---
+++ imports/api/collections.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo'
+
+export const Savia = new Mongo.Collection('savia')
--- server/main.js
+++ server/main.js
@@ -1,3 +1,24 @@
 import { print } from '../imports/tools/tools'
 
-print('Start....')
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+import { Savia } from '../imports/api/collections'
+
+Meteor.startup(ns => {
+
+    print('Meteor server Up')
+
+    app.post('/V17/savia', Meteor.bindEnvironment((req, res) =>{
+        print("REQ: " , req.body)
+        Savia.insert(req.body, (error,id) => {
+            if(!error){
+                res.sendStatus(200)
+            }
+        })
+    }))
+
+    app.listen(14555);
+
+})
