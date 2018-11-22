e8f74067eb3de844d90b247121d1479088ca1347
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2018-11-21 19:02:51 -0500

    add URIs

--- imports/api/collections.js
+++ imports/api/collections.js
@@ -1,3 +1,7 @@
 import { Mongo } from 'meteor/mongo'
-
+// Collection for test
 export const Savia = new Mongo.Collection('savia')
+// Real Data of CLient
+export const Antapaccay = new Mongo.Collection('antapaccay')
+export const Exsapropia = new Mongo.Collection('exsapropia')
+export const Induamerica = new Mongo.Collection('induamerica')
--- server/main.js
+++ server/main.js
@@ -2,17 +2,46 @@ import { print } from '../imports/tools/tools'
 
 const express = require('express')
 const app = express()
+
 app.use(express.json())
 
 import { Savia } from '../imports/api/collections'
+import { Antapaccay } from '../imports/api/collections'
+import { Exsapropia } from '../imports/api/collections'
+import { Induamerica } from '../imports/api/collections'
+
+const Savia_URI = '/V17/savia'
+const Antapaccay_URI = '/V17/antapaccay'
+const Exsapropia_URI = '/V17/exsapropia'
+const Induamerica_URI = '/V17/induamerica'
 
 Meteor.startup(ns => {
 
     print('Meteor server Up')
 
-    app.post('/V17/savia', Meteor.bindEnvironment((req, res) =>{
-        print("REQ: " , req.body)
+    app.post(Savia_URI, Meteor.bindEnvironment((req, res) =>{
         Savia.insert(req.body, (error,id) => {
+            if(!error){
+                res.sendStatus(200)
+            }
+        })
+    }))
+    app.post(Antapaccay_URI, Meteor.bindEnvironment((req, res) =>{
+        Antapaccay.insert(req.body, (error,id) => {
+            if(!error){
+                res.sendStatus(200)
+            }
+        })
+    }))
+    app.post(Exsapropia_URI, Meteor.bindEnvironment((req, res) =>{
+        Exsapropia.insert(req.body, (error,id) => {
+            if(!error){
+                res.sendStatus(200)
+            }
+        })
+    }))
+    app.post(Induamerica_URI, Meteor.bindEnvironment((req, res) =>{
+        Induamerica.insert(req.body, (error,id) => {
             if(!error){
                 res.sendStatus(200)
             }
@@ -22,3 +51,4 @@ Meteor.startup(ns => {
     app.listen(14555);
 
 })
+
