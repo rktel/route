d5437256da4cfe15508da23ef8954328aa90d7d2
rktel <rakinakuy@hotmail.com>
2019-05-06 17:46:47 -0500

    install axios Sutran 6

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -7,7 +7,7 @@ import { ST_Dinet } from "../api/streamers.js";
 import axios from 'axios'
 export default {
   mounted() {
-      stXS.on("Sutran", data => {
+      ST_Dinet.on("Sutran", data => {
           axios.post('http://190.223.32.139:14555/V17/sutran', data)
       })
   }
