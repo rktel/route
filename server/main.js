7a5dfe686e10626624991c69d5d2ddf03ce710e7
rktel <rakinakuy@hotmail.com>
2019-05-06 18:01:21 -0500

    install axios Sutran 9

--- server/main.js
+++ server/main.js
@@ -75,11 +75,11 @@ Meteor.startup(ns => {
       // axios.post('http://190.223.32.139:14555/V17/sutran', req.body)
     //  axios.post('http://190.223.32.139:14555/V17/sutran', Meteor.bindEnvironment(req.body))
        //  Meteor.call('sutran', req.body)
+       ST_Dinet.emit('Sutran', req.body)
 
         Servosa.insert(req.body, (error, id) => {
             if (!error) {
                 res.sendStatus(200)
-                ST_Dinet.emit('Sutran', req.body)
             //    Meteor.call('sutran', req.body)
            //    axios.post('http://190.223.32.139:14555/V17/sutran', req.body)
              // eventEmitter.emit('sutran', req.body);
