885a38672cd3b76c2695d65cef06aa66faa74f2b
rktel <rakinakuy@hotmail.com>
2019-05-07 17:49:42 -0500

    python process

--- server/main.py
+++ server/main.py
@@ -1,2 +1,6 @@
-print('Hello from Python')
-print('Version 2.7')
+import sys 
+# Takes first name and last name via command  
+# line arguments and then display them 
+print("Output from Python") 
+print("First name: " + sys.argv[1]) 
+print("Last name: " + sys.argv[2])
