23a45bda361f8239f3dccf174a41083750b6e0fa
rktel <rakinakuy@hotmail.com>
2019-09-27 14:42:39 -0500

    add punt client 3

--- server/main.js
+++ server/main.js
@@ -8,6 +8,16 @@ function sendData(data) {
 
  */
 
+var punt = require('punt');
+//var a = punt.connect('10.12.1.41:5000');
+// let counter = 1
+/*
+setInterval(function(){
+  a.send({ name: 'Alert name ' + counter , priority: counter });
+  counter++
+}, 2000);
+*/
+
 const express = require('express')
 const app = express()
 
