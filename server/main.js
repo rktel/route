19c105a89d282e7cb5f80b05daf07a643eab35da
rktel <rakinakuy@hotmail.com>
2019-12-27 15:02:50 -0500

    log alert 3

--- server/main.js
+++ server/main.js
@@ -127,6 +127,9 @@ Meteor.startup(ns => {
     app.post(SOC_Alerts_URI, Meteor.bindEnvironment((req, res) => {
        // console.log("SOC_ALERTS:", req.body.alerts[0])
         sendData(req.body)
+        if(req.body.version==='1.3'){
+            console.log(req.body)
+        }
         res.sendStatus(200)
     }))
     app.post(Chingudi_URI, Meteor.bindEnvironment((req, res) => {
