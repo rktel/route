9d50627a9f7f60e2835067cc3a945f4fa508b858
rktel <rakinakuy@hotmail.com>
2020-05-15 15:56:33 -0500

    GrupoQR Prodc to carcool

--- server/main.js
+++ server/main.js
@@ -4,7 +4,10 @@ const http = require('http')
 const HOST_CARCOOL = 'dev.carcool.pe'
 const PATH_CARCOOL = '/gps-rest/api/gps-tracker'
 const METHOD_CARCOOL = 'POST'
-const TOKEN_GRUPO_QR = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImZhYTk2NWJiNzVkZGUwZDUxNTBmMjk0NDA4NjhmZjA0OWZkYjcwYjE5NmE3Nzc0YmZhZTE5ZWMzZWE1NjQ0MjhhMzMyMjUyOWZhNjNjYmNjIn0.eyJhdWQiOiIxIiwianRpIjoiZmFhOTY1YmI3NWRkZTBkNTE1MGYyOTQ0MDg2OGZmMDQ5ZmRiNzBiMTk2YTc3NzRiZmFlMTllYzNlYTU2NDQyOGEzMzIyNTI5ZmE2M2NiY2MiLCJpYXQiOjE1ODg4MjAyMjksIm5iZiI6MTU4ODgyMDIyOSwiZXhwIjoxNjIwMzU2MjI5LCJzdWIiOiIxNyIsInNjb3BlcyI6W119.lKJ7kMTTjVQ29PAJTcHZPiK_7jNnRXujHcVYdmMyJXFk46pOWsaaAd1KdDe4zy9moKoOgPlD41DixK1OV9B5z7tuD7ZVlE3wSLzn86GDHFkXkvY-IvNrJjnMKp4pKKXrYDD5uWQbtpqXyBuX8wcYfv3LUlZEKTrgaQ8eLzvO5Kj8j_z8WiaCvy4RTAK7BEpVmZa3EV8tvTduTq4qkBL-kqn8ZQIDIQ1bLhtnmmCpKjbz1JekbMKzrVkZarJZwbenwUzY6cJB-unMOO0dEhxTr3zqww7J0xxbxZTwwK76bgi0N_C__E6sEE_Ohg3DLRZrdiCLHIj1BtnomWJpKQft8Bu87kbp_IwQ9dTao3bIkEhC8dWpieJ8g-UzqTMzKZOhTe3hE3iDEXLzvfURhRDtiU4QBHSzF0nGc2cKZTKZXw8n1t0_F_AHRv2HJFNXcLzIAZx_eCMhyuI4i3jfjzYNu8293gfQTe7rsEt8ssagH6gaiHqObpC6qGdYN9-OBo0ZmSlTVsPn9LSAP_RyhecAP9C36S7G3mp9vM29CY0HJZ3HVNvZYnqsDj9iS45jNnnOC8ndcf4ERFDUaGlFJXseWgDR_Nrx0_Sw_KOhGVHzJXX-Z_KhgtYRj4s1jZQDuU0TgNK6MZwtyni-NZhvipD8tRyS8kh-XTZw1zy551EWVvo'
+/**TOken DEV */
+// const TOKEN_GRUPO_QR = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImZhYTk2NWJiNzVkZGUwZDUxNTBmMjk0NDA4NjhmZjA0OWZkYjcwYjE5NmE3Nzc0YmZhZTE5ZWMzZWE1NjQ0MjhhMzMyMjUyOWZhNjNjYmNjIn0.eyJhdWQiOiIxIiwianRpIjoiZmFhOTY1YmI3NWRkZTBkNTE1MGYyOTQ0MDg2OGZmMDQ5ZmRiNzBiMTk2YTc3NzRiZmFlMTllYzNlYTU2NDQyOGEzMzIyNTI5ZmE2M2NiY2MiLCJpYXQiOjE1ODg4MjAyMjksIm5iZiI6MTU4ODgyMDIyOSwiZXhwIjoxNjIwMzU2MjI5LCJzdWIiOiIxNyIsInNjb3BlcyI6W119.lKJ7kMTTjVQ29PAJTcHZPiK_7jNnRXujHcVYdmMyJXFk46pOWsaaAd1KdDe4zy9moKoOgPlD41DixK1OV9B5z7tuD7ZVlE3wSLzn86GDHFkXkvY-IvNrJjnMKp4pKKXrYDD5uWQbtpqXyBuX8wcYfv3LUlZEKTrgaQ8eLzvO5Kj8j_z8WiaCvy4RTAK7BEpVmZa3EV8tvTduTq4qkBL-kqn8ZQIDIQ1bLhtnmmCpKjbz1JekbMKzrVkZarJZwbenwUzY6cJB-unMOO0dEhxTr3zqww7J0xxbxZTwwK76bgi0N_C__E6sEE_Ohg3DLRZrdiCLHIj1BtnomWJpKQft8Bu87kbp_IwQ9dTao3bIkEhC8dWpieJ8g-UzqTMzKZOhTe3hE3iDEXLzvfURhRDtiU4QBHSzF0nGc2cKZTKZXw8n1t0_F_AHRv2HJFNXcLzIAZx_eCMhyuI4i3jfjzYNu8293gfQTe7rsEt8ssagH6gaiHqObpC6qGdYN9-OBo0ZmSlTVsPn9LSAP_RyhecAP9C36S7G3mp9vM29CY0HJZ3HVNvZYnqsDj9iS45jNnnOC8ndcf4ERFDUaGlFJXseWgDR_Nrx0_Sw_KOhGVHzJXX-Z_KhgtYRj4s1jZQDuU0TgNK6MZwtyni-NZhvipD8tRyS8kh-XTZw1zy551EWVvo'
+/*Token PRODUC */
+const TOKEN_GRUPO_QR = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjhkOGNhYTNhNWVlNTA5N2U5MmM4YzIwM2NmNmVhNjg1MzU0MTQ3ZGU0MmYzMGRmOTI2ZGNjYmFmODg4ZWU5ZWRkYjc0YTBlMTMzMjdkMGU2In0.eyJhdWQiOiIxIiwianRpIjoiOGQ4Y2FhM2E1ZWU1MDk3ZTkyYzhjMjAzY2Y2ZWE2ODUzNTQxNDdkZTQyZjMwZGY5MjZkY2NiYWY4ODhlZTllZGRiNzRhMGUxMzMyN2QwZTYiLCJpYXQiOjE1ODk1NzU5NTEsIm5iZiI6MTU4OTU3NTk1MSwiZXhwIjoxNjIxMTExOTUxLCJzdWIiOiIxNyIsInNjb3BlcyI6W119.jvbJYwA_nukBOn_9PHTcJhLMPCSATgEO0PtahHoq64qtC9vVfnfFQ1JDg6YfPlfIRWeOhN0-jqqn5LwynzbCYFBIK6tpKBxz4utmb54xh9Nf2bOyWMHu6KN2jMaUssXTTNpxw1CodWfbb5UfEslaTijd73MdBloMdhFNQVkZc39YCNT48UWzS0UYq6xYnJObBRtEvH07m88ve5vn1lvVK04UqBNQmYV8MietgAHepLVy0hyokRyaT1-y4NLAUYS-zuiS1g3_a1ceVKnhSYdLdVQtyVHfU4p7EV36pRNEqFlA_p7sp8SQlyIv_7bl9GcScOu6Ss3ad5cuuNg9lR04dD4HfhORcD-VR_x2OspCySqGRxDFhpa4-S-x1-i-eJbIp0js2e8rsbIdjgDdPGfum7XF14hqcR-1Q_m8NW5as1GYP0KF7OoAhOvm7zGIDB3DSmwalMKTYepzwDKnOhd3Bk50e0QitdsSxt4-agj-fvNi9I-tBFdwzgjIeQMvW87ehY1Lp2N5ooqQXdOZ9cqyEMyOpCLherByfx5h3MtMVeGxkfSPOa0NnyBq92nM6yiVoybM2yyu3IalH-j8TfN9iNn4GwlKzTPfQiawnfXo6mcySyZN9VecnV35St98FXByMPEUwh3vux8UrSLTR1714I97W3-4EBblpVsa1hKtdh8'
 
 /**¿end */
 
@@ -162,41 +165,6 @@ Meteor.startup(ns => {
     app.post(Atlantic_URI, Meteor.bindEnvironment((req, res) => {
         Atlantic.insert(req.body, (error, id) => {
             if (!error) {
-
-                  /**INI */
-                let data = req.body.events[0]
-
-                let isodate = data.created
-                let date = new Date(isodate)
-                date = date.getTime() / 1000
-
-                console.log('device_timestamp:', date)
-                console.log('lat:', data.location.latitude)
-                console.log('lng:', data.location.longitude)
-                console.log('velocity:', data.location.speed)
-                console.log('imei:', data.device)
-                console.log('plate:', data.vehicle)
-
-                let digitals = data.inputs.digital
-                let ignitions = digitals.filter(item => {
-                    return item.type == 13
-                })
-                if (ignitions.length == 1) {
-                    let ignition = ignitions[0]
-                    if (ignition.value == true) {
-                        console.log('engine:', 1)
-                        sendDataToCoolcar(data.location.longitude, data.location.latitude, data.device, data.vehicle, data.location.speed, 1, date, TOKEN_GRUPO_QR)
-                    }
-
-                    else {
-                        console.log('engine:', 0)
-                        sendDataToCoolcar(data.location.longitude, data.location.latitude, data.device, data.vehicle, data.location.speed, 0, date, TOKEN_GRUPO_QR)
-                    }
-
-                }
-            
-            /**END */
-
                 res.sendStatus(200)
             }
         })
@@ -204,6 +172,40 @@ Meteor.startup(ns => {
     app.post(GrupoQR_URI, Meteor.bindEnvironment((req, res) => {
         GrupoQR.insert(req.body, (error, id) => {
             if (!error) {
+                 /**INI */
+                 let data = req.body.events[0]
+
+                 let isodate = data.created
+                 let date = new Date(isodate)
+                 date = date.getTime() / 1000
+ 
+                 console.log('device_timestamp:', date)
+                 console.log('lat:', data.location.latitude)
+                 console.log('lng:', data.location.longitude)
+                 console.log('velocity:', data.location.speed)
+                 console.log('imei:', data.device)
+                 console.log('plate:', data.vehicle)
+ 
+                 let digitals = data.inputs.digital
+                 let ignitions = digitals.filter(item => {
+                     return item.type == 13
+                 })
+                 if (ignitions.length == 1) {
+                     let ignition = ignitions[0]
+                     if (ignition.value == true) {
+                         console.log('engine:', 1)
+                         sendDataToCoolcar(data.location.longitude, data.location.latitude, data.device, data.vehicle, data.location.speed, 1, date, TOKEN_GRUPO_QR)
+                     }
+ 
+                     else {
+                         console.log('engine:', 0)
+                         sendDataToCoolcar(data.location.longitude, data.location.latitude, data.device, data.vehicle, data.location.speed, 0, date, TOKEN_GRUPO_QR)
+                     }
+ 
+                 }
+             
+             /**END */
+
                 res.sendStatus(200)
             }
         })
