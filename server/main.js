0d7ab715f7fa742e47f8a32d32856a07059713b6
rktel <rakinakuy@hotmail.com>
2020-05-15 18:23:33 -0500

    change https

--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 /* COnexion a Coolcar GrupoQR, */
-const http = require('http')
+const http = require('https')
 
 // const HOST_CARCOOL = 'dev.carcool.pe'
 // const PATH_CARCOOL = '/gps-rest/api/gps-tracker'
