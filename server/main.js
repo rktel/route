c0b7f9fd75c83c780f2b8571a529e5bfa2c05b88
rktel <rakinakuy@hotmail.com>
2020-05-15 19:15:37 -0500

    change 3

--- server/main.js
+++ server/main.js
@@ -3,7 +3,7 @@ const http = require('https')
 
 // const HOST_CARCOOL = 'dev.carcool.pe'
 // const PATH_CARCOOL = '/gps-rest/api/gps-tracker'
-const HOST_CARCOOL = 'gps-api.carcool.pe'
+const HOST_CARCOOL = 'gps-api.carcool.pe/'
 const PATH_CARCOOL = ''
 const METHOD_CARCOOL = 'POST'
 /**TOken DEV */
